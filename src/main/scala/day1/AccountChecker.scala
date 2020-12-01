18fc6b81ed353c06fe8653006e93a5fed5a8bdf6
mva13 <markvallis1987@Gmail.com>
2020-12-01 21:16:25 +0000

    Day 1 complete

--- src/main/scala/day1/AccountChecker.scala
+++ src/main/scala/day1/AccountChecker.scala
@@ -1,15 +1,52 @@
 package day1
 
-object AccountChecker extends App {
+import java.io.File
 
-  def checkSumOfTwoNumbers(a: Int, b: Int): Boolean = a + b == 2020
+import scala.annotation.tailrec
+import scala.io.{BufferedSource, Source}
+import scala.util.{Failure, Success, Try}
 
-  def checkListOfNumbers(inputList: List[Int]): Int = {
-    val headValue = inputList.head
-    inputList
-      .tail
-      .filter(checkSumOfTwoNumbers(headValue, _))
-      .map(headValue * _)
-      .head
+object AccountChecker {
+
+  def main(args: Array[String]): Unit = {
+    readReport() match {
+      case Right(report) => checkListOfNumbers(report) match {
+        case Some(result) => println(s"Two numbers did add up to 2020, their product is $result")
+        case None => println(s"No numbers in the input list add up to 2020")
+      }
+      case Left(error) => println(error)
+    }
+  }
+
+  def readReport(resourcePath: String = "/day1/input"): Either[String, List[Int]] = {
+    val file = new File(getClass.getResource(resourcePath).getPath)
+    val source: BufferedSource = Source.fromFile(file)
+    Try(source.getLines.map(_.toInt).toList) match {
+      case Success(value) =>
+        source.close
+        Right(value)
+      case Failure(_) =>
+        source.close
+        Left("Something went wrong, make sure all lines in input file are ints")
+    }
+  }
+
+  def checkSumOfTwoNumbers(a: Int, b: Int): Boolean = {
+    a + b == 2020
+  }
+
+  @tailrec
+  def checkListOfNumbers(inputList: List[Int]): Option[Int] = {
+    if (inputList.tail.size < 1) None
+    else {
+      val headValue = inputList.head
+      Try(inputList.tail
+        .filter(checkSumOfTwoNumbers(headValue, _))
+        .map(headValue * _)
+        .head) match {
+        case Success(value) => Some(value)
+        case Failure(_) => checkListOfNumbers(inputList.tail)
+      }
+    }
   }
 }
--- src/test/scala/day1/AccountCheckerSpec.scala
+++ src/test/scala/day1/AccountCheckerSpec.scala
@@ -12,6 +12,14 @@ class AccountCheckerSpec extends UnitSpecBase {
 
   "checkListOfNumbers" should "return a list of number combinations that add up to 2020" in {
     val inputList = List(2000, 10, 20, 40)
-    checkListOfNumbers(inputList) shouldBe 40000 // (2000 * 20)
+    checkListOfNumbers(inputList) shouldBe Some(40000)
+  }
+
+  "readReport" should "return a list of ints if file is formatted correctly" in {
+    readReport("/day1/testinputgood") shouldBe Right(List(2000, 10, 20, 40))
+  }
+
+  "readReport" should "return an error message if file is not correctly formatted" in {
+    readReport("/day1/testinputbad") shouldBe Left("Something went wrong, make sure all lines in input file are ints")
   }
 }
