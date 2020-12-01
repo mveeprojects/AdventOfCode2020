0d2d6430998e6f479c67881dff99106f06d58299
mva13 <markvallis1987@Gmail.com>
2020-12-01 20:07:13 +0000

    Day 1 tests passing

--- src/main/scala/day1/AccountChecker.scala
+++ src/main/scala/day1/AccountChecker.scala
@@ -1,5 +1,15 @@
 package day1
 
 object AccountChecker extends App {
-  def checkNumbers(a: Int, b: Int): Boolean = a + b == 2020
+
+  def checkSumOfTwoNumbers(a: Int, b: Int): Boolean = a + b == 2020
+
+  def checkListOfNumbers(inputList: List[Int]): Int = {
+    val headValue = inputList.head
+    inputList
+      .tail
+      .filter(checkSumOfTwoNumbers(headValue, _))
+      .map(headValue * _)
+      .head
+  }
 }
--- src/test/scala/day1/AccountCheckerSpec.scala
+++ src/test/scala/day1/AccountCheckerSpec.scala
@@ -1,15 +1,17 @@
 package day1
 
 import base.UnitSpecBase
-import day1.AccountChecker.checkNumbers
+import day1.AccountChecker._
 
 class AccountCheckerSpec extends UnitSpecBase {
 
-  behavior of "checkNumbers"
-
-  it should "return true if 2 numbers add up to 2020, otherwise return false" in {
-    checkNumbers(2019, 1) shouldBe true
-    checkNumbers(2019, 0) shouldBe false
+  "checkSumOfTwoNumbers" should "return true if 2 numbers add up to 2020, otherwise return false" in {
+    checkSumOfTwoNumbers(2019, 1) shouldBe true
+    checkSumOfTwoNumbers(2019, 0) shouldBe false
   }
 
+  "checkListOfNumbers" should "return a list of number combinations that add up to 2020" in {
+    val inputList = List(2000, 10, 20, 40)
+    checkListOfNumbers(inputList) shouldBe 40000 // (2000 * 20)
+  }
 }
