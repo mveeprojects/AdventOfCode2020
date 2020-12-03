f331c4eb9c8f618e5f93a05311e8ff5443c3426b
mva13 <markvallis1987@Gmail.com>
2020-12-03 00:16:01 +0000

    Day 2 part 2 complete

--- src/test/scala/helpers/FileReaderSpec.scala
+++ src/test/scala/helpers/FileReaderSpec.scala
@@ -5,10 +5,10 @@ import helpers.FileReader.readInputFile
 
 class FileReaderSpec extends UnitSpecBase {
   "readInputFile" should "return a list of ints if file is formatted correctly" in {
-    readInputFile("/day1/testinputgood", _.toInt) shouldBe Right(List(2000, 10, 20, 40))
+    readInputFile("/filereadertestinput/testinputgood", _.toInt) shouldBe Right(List(2000, 10, 20, 40))
   }
 
   "readInputFile" should "return an error message if file is not correctly formatted" in {
-    readInputFile("/day1/testinputbad", _.toInt) shouldBe Left("Something went wrong, make sure the input file exists at the path specified and is formatted correctly")
+    readInputFile("/filereadertestinput/testinputbad", _.toInt) shouldBe Left("Something went wrong, make sure the input file exists at the path specified and is formatted correctly")
   }
 }
